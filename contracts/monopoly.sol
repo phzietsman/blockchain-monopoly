ac232f5406cbfe46604e331e81cc118902d34cc4
Paul Zietsman <phzietsman@gmail.com>
2017-09-29 06:15:23 +0200

    Web3 wip

--- contracts/monopoly.sol
+++ contracts/monopoly.sol
@@ -15,13 +15,15 @@ contract Owned {
     function transferOwnership(address newOwner) onlyOwner {
         owner = newOwner;
     }
+
 }
 
-contract BankManager is Owned {
+contract Bank is Owned {
     address public bankManager;
+		uint256 public bankBalance = 0;
 
-    function BankManager() {
-        bankManager = msg.sender;
+    function Bank() {
+        owner = msg.sender;
     }
 
     modifier onlyBankManager {
@@ -32,216 +34,208 @@ contract BankManager is Owned {
     function newBankManager(address newManager) onlyOwner {
         bankManager = newManager;
     }
+
+    
 }
 
 contract MonopolyGame {
-    mapping (address => address) public games;
+    mapping (address => MonopolyBank) public games;
 
     function newGame (string tokenName, string tokenSymbol) {
         uint256 monopolyTotalMoneyValue = 15140;
         uint8 decimals = 0;
 
-        address newGameAddress = new MonopolyBank(monopolyTotalMoneyValue, tokenName, decimals, tokenSymbol, msg.sender);
+        MonopolyBank newGameAddress = new MonopolyBank(monopolyTotalMoneyValue, tokenName, decimals, tokenSymbol, msg.sender);
         games[msg.sender] = newGameAddress;
     } 
 }
 
-contract MonopolyBank is Owned, BankManager {
+contract MonopolyBank is Owned, Bank {
+
+		// Standard Token variables
     /* Public variables of the token */
     string public name;
     string public symbol;
     uint8 public decimals;
     uint256 public totalSupply;
    
-    address bankAddress = 0x0;
-    address fineAddress = 0x0;
-
-    struct Charm {
-        bool assigned;
-        address addr; 
-    }
-
-    mapping (string => Charm) charmOwners;
-
     /* This creates an array with all balances */
     mapping (address => uint256) public balanceOf;
 
+		// Monopoly Spesific
+    uint256 public finesBalance = 0;
+		
+		struct FineClaim {
+			address to;
+			bool approved;
+			uint expiretime;
+		}
+
+		FineClaim public activeFineClaim;
+
+		string[] public charms = [
+			"Battleship",
+			"Boot",
+      "Scottie",
+      "Iron",
+      "Racecar",
+    	"Hat",
+      "Thimble",
+      "Wheelbarrow"
+		];
+
+		mapping (string => bool) _charms;
+    mapping (string => address) charmToOwner;
+		mapping (address => string) public ownerToCharm;
+
     /* This generates a public event on the blockchain that will notify clients */
     event Transfer(string from, address indexed to, uint256 value);
+		event Paid(string from, string to, uint256 value);
+		event ClaimFines(string who);
 
-    /* Initializes contract with initial supply tokens to the creator of the contract */
     function MonopolyBank(
         uint256 initialSupply,
         string tokenName,
         uint8 decimalUnits,
         string tokenSymbol,
-        address owner
-        ) {
-        balanceOf[bankAddress] = initialSupply;                        // Update total supply
+        address ownerNew
+    ) {
+        bankBalance = initialSupply;                        // Update total supply
         totalSupply = initialSupply;                        // Update total supply
         name = tokenName;                                   // Set the name for display purposes
         symbol = tokenSymbol;                               // Set the symbol for display purposes
         decimals = decimalUnits;                            // Amount of decimals for display purposes
-        owner = owner;
 
-        resetGame();
+        // msg.sender in this context is not the same as in the MonopolyGame context,
+        // since this contract gets executed from the MonopolyGame address, the sender is 
+        // MonopolyGame.
+				transferOwnership(ownerNew);
+        newBankManager(ownerNew);
+
+				// This looks stupid, but it is way cheaper than 
+				// doing this every time you need to check a charm name
+				for (uint i = 0; i < charms.length; i++) {
+					_charms[charms[i]] = true;
+				}
     }
 
-    /* Internal transfer, only can be called by this contract */
-    function _transfer(address _from, string _fromCharm, address _to, uint _value) internal {
+		function resetGame() onlyOwner {
+			for (uint i = 0; i < charms.length; i++) {
+				
+				address toDelete = charmToOwner[charms[i]];
+				ownerToCharm[toDelete] = "Deleted";
+				charmToOwner[charms[i]] = 0x0;
+				balanceOf[toDelete] = 0;
+			}
+
+			bankBalance = totalSupply;
+			finesBalance = 0;
+			newBankManager(owner);
+		}
+
+		// The owner must be the bank manager to do this
+		// true after a reset
+		function assignPlayer(string charm, address to) onlyOwner {
+			require(_validCharm(charm));
+			ownerToCharm[to] = charm;
+			charmToOwner[charm] = to;
+			bankToCharm(charm, 1500);
+		}
+
+		modifier onlyPlayers {
+				require(_validCharm(ownerToCharm[msg.sender]));
+        _;
+    }
+
+		function _validCharm(string charmName) internal returns (bool isValid) {
+        isValid = _charms[charmName];
+				return isValid;
+    }
+    
+
+		/* Internal transfer, only can be called by this contract */
+    function _transfer(address _from, string _fromCharm, address _to, string _toCharm, uint _value) internal {
         require (balanceOf[_from] > _value);                // Check if the sender has enough
         require (balanceOf[_to] + _value > balanceOf[_to]); // Check for overflows
         balanceOf[_from] -= _value;                         // Subtract from the sender
         balanceOf[_to] += _value;                            // Add the same to the recipient
         Transfer(_fromCharm, _to, _value);
-    }
-
-    function _assignCharm(string charmName, address _to) internal onlyOwner {
-        charmOwners[charmName] = Charm({assigned:true, addr: _to});
-    }
-
-    function _payToCharm(address _from, string charmName, uint value) internal {
-        require(charmOwners[charmName].assigned);
-        _transfer(_from, charmOwners[charmName].addr, value);
-    }
-
-    function resetGame() onlyOwner {
-        balanceOf[bankAddress] = totalSupply;
-        balanceOf[fineAddress] = 0;
-        newBankManager(owner);
-
-        balanceOf[charmOwners["Battleship"].addr] = 0;
-        balanceOf[charmOwners["Boot"].addr] = 0;
-        balanceOf[charmOwners["Scottie"].addr] = 0;
-        balanceOf[charmOwners["Iron"].addr] = 0;
-        balanceOf[charmOwners["Racecar"].addr] = 0;
-        balanceOf[charmOwners["Hat"].addr] = 0;
-        balanceOf[charmOwners["Thimble"].addr] = 0;
-        balanceOf[charmOwners["Wheelbarrow"].addr] = 0;
-
-        charmOwners["Battleship"] = Charm({assigned:false, addr: 0x0});
-        charmOwners["Boot"] = Charm({assigned:false, addr: 0x0});
-        charmOwners["Scottie"] = Charm({assigned:false, addr: 0x0});
-        charmOwners["Iron"] = Charm({assigned:false, addr: 0x0});
-        charmOwners["Racecar"] = Charm({assigned:false, addr: 0x0});
-        charmOwners["Hat"] = Charm({assigned:false, addr: 0x0});
-        charmOwners["Thimble"] = Charm({assigned:false, addr: 0x0});
-        charmOwners["Wheelbarrow"] = Charm({assigned:false, addr: 0x0});
-    }
-
-    // Assigning addresses to the different charms
-    // PZ: shit way to do this
-    function setBattleship(address _to) {
-        _assignCharm("Battleship", _to);
-    }
-
-    function setBoot(address _to) {
-        _assignCharm("Boot", _to);
-    }
-
-    function setScottie(address _to) {
-        _assignCharm("Scottie", _to);
-    }
-
-    function setIron(address _to) {
-        _assignCharm("Iron", _to);
-    }
-
-    function setRacecar(address _to) {
-        _assignCharm("Racecar", _to);
-    }
-
-    function setHat(address _to) {
-        _assignCharm("Hat", _to);
-    }
-
-    function setThimble(address _to) {
-        _assignCharm("Thimble", _to);
-    }
-
-    function setWheelbarrow(address _to) {
-        _assignCharm("Wheelbarrow", _to);
+				Paid(_fromCharm, _toCharm, _value);
     }
 
 
-    // Pay to a charm
-    // PZ: shit way to do this
-    function payBattleship(uint value) {
-        _payToCharm(msg.sender, "Battleship", value);
-    }
+		// Players only paying
+		// ============================================
+		function payToCharm(string toCharmName, uint256 value) onlyPlayers {
+			require(_validCharm(toCharmName));
+			_transfer(msg.sender, ownerToCharm[msg.sender], charmToOwner[toCharmName], toCharmName, value);
+		}
 
-    function payBoot(uint value) {
-        _payToCharm(msg.sender, "Boot", value);
-    }
+		function payToBank(uint256 value) onlyPlayers {
 
-    function payScottie(uint value) {
-        _payToCharm(msg.sender, "Scottie", value);
-    }
+      require (balanceOf[msg.sender] > value);                				// Check if the sender has enough
+      require (bankBalance + value > bankBalance); 			// Check for overflows
+      balanceOf[msg.sender] -= value;                         	// Subtract from the sender
+      bankBalance += value;                            	// Add the same to the recipient
+      Paid(ownerToCharm[msg.sender], "Bank", value);			
+		}
 
-    function payIron(uint value) {
-        _payToCharm(msg.sender, "Iron", value);
-    }
+		function payToFines(uint256 value) onlyPlayers {
 
-    function payRacecar(uint value) {
-        _payToCharm(msg.sender, "Racecar", value);
-    }
+      require (balanceOf[msg.sender] > value);                				// Check if the sender has enough
+      require (finesBalance + value > finesBalance); 			// Check for overflows
+      balanceOf[msg.sender] -= value;                         	// Subtract from the sender
+      finesBalance += value;                            	// Add the same to the recipient
+      Paid(ownerToCharm[msg.sender], "Fines", value);			
+		}
 
-    function payHat(uint value) {
-        _payToCharm(msg.sender, "Hat", value);
-    }
+		// Bank paying
+		// ============================================
+		function bankToCharm(string toCharmName, uint256 value) onlyBankManager {
+			require(_validCharm(toCharmName));
 
-    function payThimble(uint value) {
-        _payToCharm(msg.sender, "Thimble", value);
-    }
+			address to = charmToOwner[toCharmName];
 
-    function payWheelbarrow(uint value) {
-        _payToCharm(msg.sender, "Wheelbarrow", value);
-    }
+      require (bankBalance > value);                				// Check if the sender has enough
+      require (balanceOf[to] + value > balanceOf[to]); 			// Check for overflows
+      bankBalance -= value;                         	// Subtract from the sender
+      balanceOf[to] += value;                            	// Add the same to the recipient
+      Transfer("Bank", to, value);
+			Paid("Bank", toCharmName, value);	
+		}
 
-    function payBank(uint value) {
-        _transfer(msg.sender, bankAddress, value);
-    }
+		// Fines
+		//  ============================================
+		function claimFine() onlyPlayers {
+			// No active claim
+			require(activeFineClaim.approved == true || now > activeFineClaim.expiretime);
+			
+			activeFineClaim = FineClaim({
+				to: msg.sender,
+				approved: false,
+				expiretime: now + 5 minutes
+			});			
 
-    function payFine(uint value) {
-        _transfer(msg.sender, fineAddress, value);
-    }
+			ClaimFines(ownerToCharm[msg.sender]);
+		}
 
-    function claimFine() {
-        _transfer(fineAddress, msg.sender, balanceOf[fineAddress]);
-    }
+		function releaseFine() onlyPlayers {
+	
+			require(activeFineClaim.approved != true || now < activeFineClaim.expiretime);
+			require(activeFineClaim.to != msg.sender);
 
-    // Pay to a charm
-    // PZ: shit way to do this
-    function payBattleshipFromBank(uint value) onlyBankManager {
-        _payToCharm(bankAddress, "Battleship", value);
-    }
 
-    function payBootFromBank(uint value) onlyBankManager {
-        _payToCharm(bankAddress, "Boot", value);
-    }
+			string storage to = ownerToCharm[activeFineClaim.to];
+			uint256 value = finesBalance;
 
-    function payScottieFromBank(uint value) onlyBankManager {
-        _payToCharm(bankAddress, "Scottie", value);
-    }
+      require (balanceOf[activeFineClaim.to] + finesBalance > balanceOf[activeFineClaim.to]); 			// Check for overflows
+      balanceOf[activeFineClaim.to] += finesBalance;                            	// Add the same to the recipient
+      finesBalance = 0;                         								// Subtract from the sender
 
-    function payIronFromBank(uint value) onlyBankManager {
-        _payToCharm(bankAddress, "Iron", value);
-    }
+			activeFineClaim.approved = true;
 
-    function payRacecarFromBank(uint value) onlyBankManager {
-        _payToCharm(bankAddress, "Racecar", value);
-    }
+      Transfer("Fines", activeFineClaim.to, value);
+			Paid("Fines", to, value);	
+		}
 
-    function payHatFromBank(uint value) onlyBankManager {
-        _payToCharm(bankAddress, "Hat", value);
-    }
-
-    function payThimbleFromBank(uint value) onlyBankManager {
-        _payToCharm(bankAddress, "Thimble", value);
-    }
-
-    function payWheelbarrowFromBank(uint value) onlyBankManager {
-        _payToCharm(bankAddress, "Wheelbarrow", value);
-    }
-    
 }
